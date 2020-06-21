e6729877eb5e535b923ce191f7391a12d583c00e
Shi Han Qin <shihanqin@Shis-MacBook-Pro.local>
2020-06-20 23:27:26 -0400

    Something wrong with axios get calls

--- api.js
+++ api.js
@@ -1,31 +1,25 @@
 import axios from "axios";
 
 export default {
-  getToken: (code) => {
-    const data = new FormData();
-    data.append("client_id", "857366711422868");
-    data.append("client_secret", "a69514fe451d7acec9cd710fa9d102a3");
-    data.append("grant_type", "authorization_code");
-    data.append("redirect_uri", "https://f6d65732caf2.ngrok.io/");
-    data.append("code", code);
-
-    const headers = {
-      "Content-Type": "multipart/form-data",
-    };
-
-    axios.post(
-      "https://cors-anywhere.herokuapp.com/https://api.instagram.com/oauth/access_token",
-      data,
-      headers
+  getUsername: (token) => {
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/https://graph.instagram.com/me?fields=username&access_token=" +
+        token,
+      { headers: { origin: "" } }
     );
-    // .then(
-    //   (response) => {
-    //     const accessToken = response.data["access_token"];
-    //     const userID = response.data["user_id"];
-    //   },
-    //   (error) => {
-    //     console.log(error);
-    //   }
-    // );
+    // .then((res) => {
+    //   console.log("getusernameres" + res);
+    //   console.log("getusernameres" + JSON.stringify(res.data.username));
+    // })
+    // .catch((err) => console.log("getusernamerr" + err));
+  },
+  getMedia: (token) => {
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/https://graph.instagram.com/me/media?fields=id&access_token=" +
+        token,
+      { headers: { origin: "" } }
+    );
+    // .then((res) => console.log(res))
+    // .catch((err) => console.log(err));
   },
 };
